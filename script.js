53abf45ccfdcc3ffb28a181de00e845c52adae97
MarVasilieva <marina_vasilieva@list.ru>
2022-12-01 11:35:53 +0600

    first cod on js

---
+++ script.js
@@ -0,0 +1,29 @@
+function showName(){
+    let name= prompt ('Познакомимся,красотка?')
+    alert ('Я не против,меня зовут -'  + name);
+}
+function showSumm()
+{
+    let x= +prompt ('Input first number');
+    let y= +prompt ('Input second number');
+    alert(x+y);
+    }
+function showSubtraction()
+    {
+        let x= +prompt ('Input first number');
+        let y= +prompt ('Input second number');
+        alert(x-y);
+        }
+function showMultiplication()
+        {
+            let x= +prompt ('Input first number');
+            let y= +prompt ('Input second number');
+            alert(x*y);
+            }
+function showSegmentation()
+            {
+                let x= +prompt ('Input first number');
+                let y= +prompt ('Input second number');
+                alert(x/y);
+                }  
+                
